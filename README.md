33678d859d93d992fb263ab2f5271f8a95baf339
cobraguy <cobraguy@users.noreply.github.com>
2016-01-20 10:51:14 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Space-Pea-Dodger
-A recreation of my Space Pea Dodger game in Phaser
+A recreation of my [Space Pea Dodger game](https://scratch.mit.edu/projects/10054599/) in [Phaser](http://phaser.io/).
+[Abra](https://aurifexlabs.com/) was used to develop and test the game.
+
+The game is playable on desktop. It should be playable on mobile browsers, however, I have only tested it on my Nexus 7 2013.
+The only difference between desktop and mobile is that on desktop, if your mouse leaves the game, it's game over. On mobile you can lift your finger, but it doesn't save you from losing.
+___
+
+There are still a couple things I want to do.
+
+1. Add instructions and a game over message (Simply, "Game Over", if they lost, and another message if the mouse left the game)
+2. Add a button to play again instead of having to refresh the page
+3. **Clean up the code and add comments**
---
+++ game.js
@@ -0,0 +1,155 @@
+var score;
+var bootState = {
+    create: function(){
+        game.stage.backgroundColor = "#0c00ff";
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		game.scale.pageAlignHorizontally = true;
+		game.scale.pageAlignVertically = true;
+        game.state.start("load");
+    }
+};
+
+var loadState = {
+    preload: function(){
+        var loadingLabel = game.add.text(game.world.centerX, game.world.centerY, "Loading...", {fill: "#ffffff"});
+        loadingLabel.align = "center";
+        loadingLabel.anchor.setTo(0.5);
+        game.load.image("pea", "pea.png");
+        game.load.image("player", "player-point.png");
+        game.load.spritesheet("button", "button.png", 200, 75);
+    },
+    
+    create: function(){
+        game.state.start("menu");
+    }
+};
+
+var menuState = {
+    create: function(){
+        this.button = game.add.button(game.world.centerX, game.world.centerY, "button", this.buttonClicked, this, 1, 0);
+        this.button.anchor.setTo(0.5);
+    },
+    
+    buttonClicked: function(){
+        game.state.start("play");
+    }
+};
+
+var playState = {
+    create: function(){
+        score = 0;
+        this.target = score + game.rnd.integerInRange(5, 10);
+        this.scoreLabel = game.add.text(game.world.centerX, 20, score, {fill: "#ffffff", font: "30px Calibri"});
+        this.scoreLabel.align = "center";
+        this.scoreLabel.anchor.setTo(0.5);
+
+        this.player = game.add.sprite(0, 0, "player");
+        game.physics.arcade.enable(this.player);
+
+        this.peas = game.add.group();
+        
+        this.createPea(2);
+    },
+    
+    update: function(){
+        if(!game.input.activePointer.withinGame) game.state.start("gameover");
+        this.player.x = game.input.x;
+        this.player.y = game.input.y;
+        game.physics.arcade.overlap(this.player, this.peas, this.onHit, null, this);
+        for(var i = 0; i < this.peas.children.length; i++){
+            var pea = this.peas.children[i];
+            if(!pea.inMotion) pea.rotation = game.physics.arcade.angleToPointer(pea);
+            
+            if(game.time.now > pea.attackTime && !pea.inMotion){
+                game.physics.arcade.moveToPointer(pea, 300);
+                pea.inMotion = true;
+            }
+            
+            if(!pea.inWorld){
+                this.stopMoving(pea);
+                score++;
+                this.scoreLabel.text = score;
+                if(pea.body.x < 0){
+                    pea.body.x = 0;
+                }else if(pea.body.x > 480){ 
+                    pea.body.x = 448;
+                }
+                
+                if(pea.body.y < 0){
+                    pea.body.y = 0;
+                }else if(pea.y > 360){
+                    pea.body.y = 328;
+                }
+                this.setAttackTime(pea);
+            }
+        }
+        
+        if(this.target <= score){
+            this.target += game.rnd.integerInRange(5, 10);
+            this.createPea();
+        }
+    },
+    
+    onHit: function(){
+        game.state.start("gameover");
+    },
+    
+    createPea: function(amount){
+        this.amount = amount || 1;
+        for(var i = 0; i < this.amount; i++){
+            var pea = this.peas.create(0, 0, "pea");
+            this.setRandPos(pea);
+            pea.anchor.setTo(0.5);
+            pea.inMotion = false;
+            game.physics.arcade.enable(pea);
+            this.setAttackTime(pea);
+        }
+    },
+    
+    setAttackTime: function(sprite){
+        sprite.attackTime = game.time.now + (game.rnd.integerInRange(1, 3) * 1000);
+    },
+    
+    stopMoving: function(sprite){
+        sprite.body.velocity.x = 0;
+        sprite.body.velocity.y = 0;
+        sprite.inMotion = false;
+    },
+    
+    setRandPos: function(sprite){
+        if(game.rnd.integerInRange(1, 2) === 1){
+            if(game.rnd.integerInRange(1, 2) === 1){
+                sprite.x = 16;
+            }else{
+                sprite.x = 464;
+            }
+            sprite.y = game.rnd.integerInRange(16, 344);
+        }else{
+            if(game.rnd.integerInRange(1, 2) === 1){
+                sprite.y = 16;
+            }else{
+                sprite.y = 344;
+            }
+            sprite.x = game.rnd.integerInRange(16, 464);
+        }
+    }
+};
+
+var gameoverState = {
+    create: function(){
+        this.endLabel = game.add.text(game.world.centerX, game.world.centerY, score, {fill: "#ffffff"});
+        this.endLabel.align = "center";
+        this.endLabel.anchor.setTo(0.5);
+    }
+};
+
+var game = new Phaser.Game(480, 360, Phaser.AUTO);
+
+game.state.add("boot", bootState);
+game.state.add("load", loadState);
+game.state.add("menu", menuState);
+game.state.add("play", playState);
+game.state.add("gameover", gameoverState);
+
+game.state.start("boot");
